59dade66fa6cbf8a4600b12c84fb89a24b8f0ac7
Blaizculo <jaime.lopez16@alu.uclm.es>
2025-10-06 10:59:23 +0200

    4 añadir clase fabricante (#25)
    * He añadido las clases Oferta, OfertaItem, Fabricante y el enum tiposDirigidaOferta
    
    * He corregido el Data
    
    * He corregido el Data

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,6 +7,9 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     DbSet<Alquiler> Alquiler { get; set; }
     DbSet<AlquilarItem> AlquilarItem { get; set; }
+    DbSet<Fabricante> Fabricante { get; set; }
+    DbSet<Oferta> Oferta { get; set; }
+    DbSet<OfertaItem> OfertaItem { get; set; }
     DbSet<Herramienta> Herramienta { get; set; }
     DbSet<Compra> Compra { get; set; }
     DbSet<CompraItem> CompraItem { get; set; }
