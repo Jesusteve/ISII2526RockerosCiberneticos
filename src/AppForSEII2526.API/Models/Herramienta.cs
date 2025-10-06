4bdfed3d4051208729f36bbee66ad220b839db37
daankk17 <maildedanielg@gmail.com>
2025-10-06 10:16:11 +0200

    He creado la clase herramienta sin las listas ya que da error si lo pongo por ahora (#20)

---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AppForSEII2526.API.Models;
+
+public class Herramienta {
+
+    public Herramienta()
+    {
+        material = "ninguno";
+        nombre = "ninguno";
+    }
+    public Herramienta(string Material, string Nombre, float Precio, float TiempoReparacion) {
+        material = Material;
+        nombre = Nombre;
+        precio = Precio;
+        tiempoReparacion = TiempoReparacion;
+    }
+
+    public int Id { get; set; }
+
+    [StringLength(50, ErrorMessage = "El nombre del material debe de tener minimo 10 caracteres y máximo 50", MinimumLength=10)]
+    public string material { get; set; }
+
+    [StringLength(50, ErrorMessage = "El nombre de la herramienta debe de tener minimo 10 caracteres y máximo 50", MinimumLength=10)]
+    public string nombre { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "El precio no puede ser negativo")]
+    public float precio { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "El tiempo no puede ser negativo")]
+    public float tiempoReparacion { get; set; }
+}
