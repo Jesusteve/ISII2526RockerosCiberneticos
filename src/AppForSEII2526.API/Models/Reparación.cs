b4bd38a72c40b0784c98cbc83c624f8ad8c78566
Daniel2004755 <Daniel.Balan@alu.uclm.es>
2025-10-13 10:12:52 +0200

    Corrección errores (#44)

--- src/AppForSEII2526.API/Models/Reparación.cs
+++ src/AppForSEII2526.API/Models/Reparación.cs
@@ -7,41 +7,23 @@ namespace AppForSEII2526.API.Models
     {
         public Reparación()
         {
-            apellidoCliente = "";
-            nombreCliente = "";
-            telefono = "";
             fechaEntrega = DateTime.Now;
             fechaRecogida = DateTime.Now.AddDays(7);
             precioTotal = 0;
         }
 
-        public Reparación(int id, string nombreCliente, string apellidoCliente, string telefono, DateTime fechaEntrega, DateTime fechaRecogida, float precioTotal, TiposMetodosPago metodoPago)
+        public Reparación(int id, string nombreCliente, string apellidoCliente, string telefono, DateTime fechaEntrega, DateTime fechaRecogida, float precioTotal, ApplicationUser applicationUser, TiposMetodosPago metodoPago)
         {
             this.id = id;
-            this.nombreCliente = nombreCliente;
-            this.apellidoCliente = apellidoCliente;
-            this.telefono = telefono;
             this.fechaEntrega = fechaEntrega;
             this.fechaRecogida = fechaRecogida;
             this.precioTotal = precioTotal;
             this.metodoPago = metodoPago;
+            this.applicationUser = applicationUser;
         }
 
         [Key]
         public int id { get; set; }
-
-        [Required(ErrorMessage = "El nombre del cliente es obligatorio")]
-        [Display(Name = "Nombre del cliente")]
-        public string nombreCliente { get; set; }
-
-        [Required(ErrorMessage = "El apellido del cliente es obligatorio")]
-        [Display(Name = "Apellido del cliente")]
-        public string apellidoCliente { get; set; }
-
-        [Phone(ErrorMessage = "Número de teléfono no válido")]
-        [StringLength(12, MinimumLength = 9, ErrorMessage = "El teléfono debe tener entre 9 y 12 caracteres")]
-        [Display(Name = "Teléfono")]
-        public string? telefono { get; set; }
 
         [Required(ErrorMessage = "La fecha de entrega es obligatoria")]
         [Display(Name = "Fecha de entrega")]
@@ -63,6 +45,8 @@ namespace AppForSEII2526.API.Models
         
 
         public List<ReparaciónItem> ReparaciónItem { get; set; }
+
+        public ApplicationUser applicationUser { get; set; }
 
         public enum TiposMetodosPago
         {
--- src/AppForSEII2526.API/Models/ReparaciónItem.cs
+++ src/AppForSEII2526.API/Models/ReparaciónItem.cs
@@ -10,14 +10,14 @@ namespace AppForSEII2526.API.Models
             cantidad = 0;
             precio = 0;
             descripcion = "Sin descripción";
-            idReparacion = 0;
-            idHerramienta = 0;
+            ReparacionId = 0;
+            HerramientaId = 0;
         }
 
         public ReparaciónItem(int reparacionId, int herramientaId, int cantidad, float precio, string descripcion)
         {
-            this.idReparacion = reparacionId;
-            this.idHerramienta = herramientaId;
+            this.ReparacionId = reparacionId;
+            this.HerramientaId = herramientaId;
             this.cantidad = cantidad;
             this.precio = precio;
             this.descripcion = descripcion;
@@ -26,11 +26,15 @@ namespace AppForSEII2526.API.Models
         [Key]
         public int id { get; set; }
 
+        public Herramienta herramienta { get; set; }
+
+        public Reparación reparacion { get; set; }
+
         [ForeignKey("Reparacion")]
-        public int idReparacion { get; set; }
+        public int ReparacionId { get; set; }
 
         [ForeignKey("Herramienta")]
-        public int idHerramienta { get; set; }
+        public int HerramientaId { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
         public int cantidad { get; set; }
@@ -41,12 +45,6 @@ namespace AppForSEII2526.API.Models
         [StringLength(200, ErrorMessage = "La descripción debe tener entre 5 y 200 caracteres", MinimumLength = 5)]
         public string? descripcion { get; set; }
 
-        
-
-        [ForeignKey(nameof(idHerramienta))]
-        public Herramienta herramienta { get; set; }
-
-        [ForeignKey(nameof(idReparacion))]
-        public Reparación reparacion { get; set; }
+       
     }
 }
