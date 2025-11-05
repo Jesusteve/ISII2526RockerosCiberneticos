adac6e6bad43d370fc297992a8a877d51f4be9af
Blaizculo <jaime.lopez16@alu.uclm.es>
2025-11-05 12:36:34 +0100

    He hecho el metodo de seleccion (#75)

--- src/AppForSEII2526.API/Controllers/HerramientaController.cs
+++ src/AppForSEII2526.API/Controllers/HerramientaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -38,8 +39,36 @@ namespace AppForSEII2526.API.Controllers
             logger.LogInformation("Finalizando GetHerramienParaComprar");
             return Ok(herramientas);
         }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramienParaAlquilarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientasForRenting_DTO(string? nombre, string? material)
+        {
+            DateTime pasMñn = DateTime.Now.AddDays(2), semSig = DateTime.Now.AddDays(7);
 
-
-
+            IList<HerramienParaAlquilarDTO> selectherr = await _context.Herramienta
+                .Include(alq => alq.AlquilarItems).ThenInclude(al => al.alquiler)
+                    .Where(alq => (nombre == null || alq.nombre.Contains(nombre))
+                    && (material == null || alq.material.Contains(material)
+                    && (alq.AlquilarItem.alquiler.fechaInicio.Date > semSig.Date && alq.AlquilarItem.alquiler.fechaFin.Date < pasMñn.Date)))
+                    .OrderBy(alq => alq.nombre)
+                    .Select(alq => new HerramienParaAlquilarDTO(alq.Id, alq.material, alq.nombre, alq.precio, alq.fabricante.nombre))
+                    .ToListAsync();
+            return Ok(selectherr);
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientaparaOfertaDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetHerramientasParaOferta(string? fabricante, float? precio)
+        {
+            IList<HerramientaparaOfertaDTO> selectherr = await _context.Herramienta
+                .Include(Ofer=> Ofer.OfertaItem).ThenInclude(Ofe => Ofe.oferta)
+                    .Where(Ofer => (fabricante == null || Ofer.nombre.Contains(fabricante))
+                    && (precio == null || Ofer.precio==precio))
+                    .OrderBy(Ofer => Ofer.nombre)
+                    .Select(Ofer=> new HerramientaparaOfertaDTO(Ofer.Id, Ofer.material, Ofer.nombre, Ofer.precio, Ofer.fabricante.nombre))
+                    .ToListAsync();
+            return Ok(selectherr);
+        }
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/HerramientaparaOfertaDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class HerramientaparaOfertaDTO
+    {
+        public HerramientaparaOfertaDTO(int Id, string material, string nombre, float precio, string fabricante)
+        {
+            this.Id = Id;
+            this.material = material;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.fabricante = fabricante;
+        }
+        public int Id { get; set; }
+        public string material { get; set; }
+        public string nombre { get; set; }
+        public float precio { get; set; }
+        public string fabricante { get; set; }
+
+    }
+}
